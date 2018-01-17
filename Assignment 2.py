3f196f2073952d44319e5f02d7e4876f0815d95f
johnnyheineken <janhynek@gmail.com>
2018-01-17 16:06:20 +0100

    Ex 1

---
+++ Assignment 2.py
@@ -0,0 +1,77 @@
+#%%
+import numpy as np
+from numpy import zeros, random, sqrt, mean
+from numpy import transpose as t
+from numpy.linalg import inv
+from matplotlib import pyplot as plt
+a_list = [1, 0.6, 0.3, 0.15, 0.07, 0.04, 0.02, 0]
+rho_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+N=100
+k=10
+beta = 0
+REP = 1000
+
+#%%
+'''
+EXERCISE 1
+'''
+
+Z = random.normal(size = (N,k))
+
+results = zeros((len(a_list), len(rho_list)))
+counter_a = 0
+for a in a_list:
+    PI = zeros(shape=k)
+    PI[0] = a
+    counter_rho = 0
+    for rho in rho_list:
+        SIGMA = np.array([[1, rho], [rho, 1]])
+        tstat_result = zeros(REP)
+        random.seed(2110)
+
+        for i in range(REP):
+            
+            eps_V = random.multivariate_normal(mean=[0, 0], cov=SIGMA, size=N)
+            epsilon = eps_V[:, 0]
+            V = eps_V[:, 1]
+            X = Z @ PI + V
+            Y = X * beta + epsilon
+            
+            PI_hat = inv(t(Z) @ Z) @ t(Z) @ X
+            X_hat = Z @ PI_hat
+            beta_hat = (1 / (t(X_hat) @ X_hat)) * t(X_hat) @ Y
+            s_2 = (t(Y - X * beta_hat) @ (Y - X * beta_hat)) / (N - 1)
+            var = s_2 * (1 / (t(X_hat) @ X_hat))
+            tstat_result[i] = beta_hat / sqrt(var)
+        rej_rate = mean((tstat_result > 1.96) | (tstat_result < -1.96))
+        results[counter_a, counter_rho] = rej_rate
+        print(rej_rate)
+        counter_rho += 1
+    counter_a += 1
+
+print(results)
+#%%
+
+
+num_plots = results.shape[0]
+colormap = plt.cm.gist_ncar
+plt.gca().set_color_cycle([colormap(i)
+                           for i in np.linspace(0, 0.9, num_plots)])
+
+labels = []
+for i in range(num_plots):
+    plt.plot(rho_list, results[i, :])
+    labels.append('a = ' + str(a_list[i]))
+
+plt.legend(labels)
+plt.ylabel('value of rho')
+plt.xlabel('rejection frequency')
+plt.show()
+
+
+#%%
+'''
+EXERCISE 2
+'''
+k = 10
+grid = [i * 0.25 for i in range(1000)]
